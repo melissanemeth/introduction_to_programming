49783046dbbcb334b478e1f223bd64ad17feb8aa
Melissa Nemeth <melissanemeth1@gmail.com>
2015-12-27 20:56:05 -0500

    Loops exercises

---
+++ Loops/each_with_index.rb
@@ -0,0 +1,5 @@
+books = ["Brave New World", "Anna Karenina", "Crime and Punishment", "Pride and Prejudice"]
+
+books.each_with_index do | book, index |
+  puts "#{index}. #{book}"
+end
---
+++ Loops/while_loop.rb
@@ -0,0 +1,8 @@
+x = ""
+
+while x != "Stop" do
+  puts "Hello, what is your name?"
+  answer = gets.chomp
+  puts "Enter 'Stop' to end loop"
+  x = gets.chomp
+end
